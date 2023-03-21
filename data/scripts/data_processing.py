8a678fb3987998b7478258e452f6d39a79232946
Yamileth Rodriguez <46757368+yrodriguez83@users.noreply.github.com>
2023-03-21 03:27:35 -0500

    Update data_processing.py

--- data/scripts/data_processing.py
+++ data/scripts/data_processing.py
@@ -2,55 +2,6 @@ import csv
 import requests
 from pymongo import MongoClient
 
-# Define the URLs of the raw data files
-COCkTAILS_URL = "https://example.com/cocktails.csv"
-INGREDIENTS_URL = "https://example.com/ingredients.csv"
-
-# Define the database connection parameters
-DB_NAME = "cocktail_recipes"
-DB_HOST = "localhost"
-DB_PORT = 27017
-
-# Connect to the database
-client = MongoClient(DB_HOST, DB_PORT)
-db = client[DB_NAME]
-
-# Define the data models for the application
-
-
-class Cocktail:
-    def __init__(self, name, image_url, ingredients):
-        self.name = name
-        self.image_url = image_url
-        self.ingredients = ingredients
-
-
-class Ingredient:
-    def __init__(self, name, unit, category):
-        self.name = name
-        self.unit = unit
-        self.category = category
-
-
-# Collect and process the raw cocktail data
-cocktail_data = []
-with requests.get(COCKTAILS_URL) as r:
-    reader = csv.DictReader(r.content.decode('utf-8').splitlines())
-    for row in reader:
-        name = row["name"]
-        image_url = row["image_url"]
-        ingredient_names = row["ingredients"].split(", ")
-        ingredients = []
-        for ingredient_name in ingredient_names:
-            ingredient = db.ingredients.find_one({"name": ingredient_name})
-            if not ingredient:
-                ingredient = Ingredient(
-                    name=ingredient_name, unit="", category="")
-                db.ingredients.insert_one(ingredient.__dict__)
-            ingredients.append(ingredient.__dict__)
-        cocktail = Cocktail(name=name, image_url=image_url,
-                            ingredients=ingredients)
-        cocktail_data.append(cocktail.__dict__)
 
 # Collect and process the raw ingredient data
 ingredient_data = []
